ebac525b71c534de98ef2e95bb97e79ab8722be4
Vaibhavdesai99 <vaibhavdesai510@gmail.com>
2023-05-14 12:13:59 +0530

    Comment Added-CreatePortal

--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
+// Backdrop is only for to make the the background Blur And Black.
 const Backdrop = (props) => {
   return <div className={classes.backdrop}></div>;
 };
 
+//ModalOverlay component is used to wrap the props.children and represent the content of the modal
 const ModalOverlay = (props) => {
   return (
     <div className={classes.modal}>
@@ -17,6 +19,7 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <>
+      {/* here in ReactDom.createportal basically give two arguments 1st what to render and 2nd where to reder */}
       {ReactDOM.createPortal(<Backdrop />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
